d8788b077c429650c45b0b6b43e9eb30992780dc
Heriniaina <andriananjamarotafika@gmail.com>
2026-09-05 20:56:18 +0300

    Exo3 Done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        do
+        {
+             Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            bool isGuessed = false;
+            int guessCount = 0;
+            while (!isGuessed)
+            {
+                Console.WriteLine("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                    isGuessed = true;
+                }
+            }
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            response = Console.ReadLine();
+        }while(response == "yes");
+       
     }
 }
