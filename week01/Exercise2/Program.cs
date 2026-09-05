385e730e9fb60ecb05e8eaf0b0ede20e8912f985
Heriniaina <andriananjamarotafika@gmail.com>
2026-09-05 20:28:05 +0300

    Exo 2 Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your Grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        string sign;
+        int lastDigit = grade % 10;
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (grade >= 97)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (grade >= 90)
+        {
+            Console.WriteLine($"You got an A{sign}!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"You got a B{sign}!");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"You got a C{sign}!");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"You got a D{sign}!");
+        }
+        else
+        {
+            Console.WriteLine("You got an F!");
+        }
     }
 }
